188d1b1d1a5051f9d286fbc807208e8cead4edaa
Sree <mulelearner1234@gmail.com>
2019-08-22 21:15:16 -0400

    Kafka Producer API added....

---
+++ KafkaNetCoreAPI/KafkaNetCoreAPI/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaNetCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ProducerConfig _config;
+        private string topic;
+
+        public ValuesController(KafkaConfiguration config)
+        {
+            _config = config;
+            topic = config.topic;
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post( string message)
+        {
+            KafkaWriter writer = new KafkaWriter(_config, topic);
+            writer.Writer(message);
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
